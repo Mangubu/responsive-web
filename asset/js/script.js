c481673b90d2982929d9d01a1aabfecdf961f8b3
Mangubu <mangubuj@gmail.com>
2016-04-15 16:43:45 +0200

    html done js done add svg picture

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,27 @@
-var burger = document.getElementById("burger");
+var $burger = document.getElementById("burger");
+var $cross = document.getElementById("cross");
+var $cross2 = document.getElementById("cross2");
+var $search = document.getElementById("search");
 
-burger.addEventListener("click", toggle, false);
+var head = document.getElementById("head");
+var side = document.getElementById("side");
+var research = document.getElementById("head-search");
 
 function toggle(){
-  var side = document.getElementById("side");
   side.classList.toggle("visible");
+  research.classList.remove("display");
+  head.classList.remove("show");
 }
+
+function search(){
+  research.classList.toggle("display");
+  head.classList.toggle("show");
+  side.classList.remove("visible");
+  head.classList.remove("visible");
+
+}
+
+$burger.addEventListener("click", toggle, false);
+$cross.addEventListener("click", toggle, false);
+$cross2.addEventListener("click", search, false);
+$search.addEventListener("click", search, false);
